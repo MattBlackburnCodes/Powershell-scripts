b332504ed18df2cb8e1afdb8386e00f650ab12d2
MB <105514525+SoliBlack@users.noreply.github.com>
2022-11-07 16:28:32 -0500

    Update Passcode Randomizer.ps1

--- Passcode Randomizer.ps1
+++ Passcode Randomizer.ps1
@@ -1,11 +1,16 @@
+# Script to pick out a number randomly from the min and max value the user inputs
+ 
 echo "This is a passcode randomizer."
-sleep 2
 echo "You must insert the minimum and maximum range"
-sleep 2
+# Get min value
 $min = Read-Host "What is the min value?"
+
+# Get max value
 $max = Read-Host "What is the max value?"
 
+# Pick a random number from the two previous values
 $randNum = Get-Random -Minimum $min -Maximum $max
 
+# Write to screen
 Write-Output "$randNum"
 Read-Host "Press enter to close window"
