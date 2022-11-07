450c71d1cae1a518f74aa3edcca2c91c9721b98f
MB <105514525+SoliBlack@users.noreply.github.com>
2022-11-07 10:03:10 -0500

    Add files via upload

---
+++ NetworkDiag.ps1
@@ -0,0 +1,35 @@
+﻿# Create a variable that for c:\Temp
+$tempFolder = 'C:\Temp'
+#If c:\temp isn't created, make folder.
+if (Test-Path -Path $tempFolder){
+    Write-OutPut "Temp folder exists"
+
+}
+
+#If the c:\ temp folder print "folder exists".
+else{
+    New-Item -Path $tempFolder -ItemType Directory
+    Write-Output "Temp folder created"
+}
+
+# Gets the date and time of when the script was executed and prints to NetDiag.txt file.
+Get-Date >> C:\Temp\NetDiag.txt
+
+# ipconfig gets the current ip address and makes a file called  netdiag.txt
+ipconfig >> C:\Temp\NetDiag.txt
+
+# Does a traceroute to google's dns server and appends to netdiag.txt file
+tracert 8.8.8.8 >> C:\Temp\NetDiag.txt
+
+# Does a traceroute to an internal resource (can't remember what exactly), and appeds to netdiag.txt file.
+tracert 10.142.3.50 >> C:\Temp\NetDiag.txt
+
+# Displays active TCP connections and ports on which the computer is listening. 
+#netstat -n (Displays active TCP connections, however, addresses and port numbers are expressed numerically and no attempt is made to determine names.)
+#netstat -o (Displays active TCP connections and includes the process ID (PID) for each connection. You can find the application based on the PID on the Processes tab in Windows Task Manager. )
+netstat -n -o >> C:\Temp\Netstat.csv
+
+# Gets the processes that are running on the local computer. 
+Get-Process >> C:\Temp\PIDinfo.csv
+
+Write-Output "Script completed"
---
+++ Passcode Randomizer.ps1
@@ -0,0 +1,10 @@
+﻿echo "This is a passcode randomizer."
+sleep 2
+echo "You must insert the minimum and maximum range"
+sleep 2
+$min = Read-Host "What is the min value?"
+$max = Read-Host "What is the max value?"
+
+$randNum = Get-Random -Minimum $min -Maximum $max
+
+Write-Output "$randNum"
